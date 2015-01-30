85c7c5bb854a108da0f160af4ff8ab3f4afee5fb
David Larrimore <davidlarrimore@gmail.com>
2015-01-29 22:22:57 -0500

    j

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var cylon = require('./routes/cylon');
+var mraa = require('./routes/mraa');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/cylon', cylon);
+app.use('/mraa', mraa);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

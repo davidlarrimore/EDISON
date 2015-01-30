de67da3260b24ff3d6015838c5f538101f11eb02
David Larrimore <davidlarrimore@gmail.com>
2015-01-30 08:36:16 -0500

    k

--- routes/mraa.js
+++ routes/mraa.js
@@ -6,10 +6,13 @@ console.log('MRAA Version: ' + mraa.getVersion());
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var myOnboardLed = new mraa.Gpio(13);
-    var ledState = myOnboardLed.read();
+    var myOnboardLed = '';
+    var ledState = 0
     var ledMessage = '';
-    console.log('ledState Before = ' + ledState); //write the mraa version to the Intel XDK
+    
+    
+    myOnboardLed = new mraa.Gpio(13);   
+    ledState = myOnboardLed.read();
 
     //inverting signal
     if(ledState == 0){
